89e370004526a6a65604b9962efbd7288fce986e
Zac Bohon <startswithz@gmail.com>
2017-03-18 10:50:59 -0500

    Implemented listener.

--- ingest/listner.py
+++ ingest/listner.py
@@ -1,7 +1,50 @@
 """
-This module will listen to an email address and call Scraper.scrap once it received email
+This module will listen to an email address and call Scraper.scrap once it
+received email
 """
 
+from Scraper import scrap
+import Thread
+import poplib
+from email import parser
+# def listen_notification():
+#    pass
 
-def listen_notification():
-    pass
+
+class EmailParser(object):
+    def __init__(self, host, user, password):
+        self.host = host
+        self.user = user
+        self.password = password
+
+    def GetEmails(self):
+        pop_conn = poplib.POP3_SSL(self.host)
+        pop_conn.user(self.user)
+        pop_conn.pass_(self.password)
+        msg_count = len(pop_conn.list()[1])
+        messages = [pop_conn.retr(i) for i in range(1, msg_count + 1)]
+        # Concat message pieces:
+        messages = ["\n".join(mssg[1]) for mssg in messages]
+        # Parse message intom an email object:
+        messages = [parser.Parser().parsestr(mssg) for mssg in messages]
+        pop_conn.quit()
+        return messages
+
+
+class EmailMonitor(Thread):
+    def __init__(self, event, scrapeFunction, emailParser):
+        Thread.__init__(self)
+        self.stopped = event
+        self.scrape = scrapeFunction
+        self.emailParser = emailParser
+
+    def run(self):
+        while not self.stopped.wait(3600.0):
+            messages = self.emailParser.GetEmails()
+            for msg in messages:
+                self.scrape(msg)
+
+
+if __name__ == '__main__':
+    p = EmailParser('pop.gmail.com', 'indivisible_bot', '!(ggL"JDF=6E@XbF')
+    EmailMonitor(None, scrap, p)
