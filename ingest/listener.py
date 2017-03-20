a4348e8d3108aceae7918b048bffbd1240de018c
Zac Bohon <startswithz@gmail.com>
2017-03-19 20:46:57 -0500

    refactored to use IMAP. Removed threading stuff as this will be implemented later in a separate issue.

--- ingest/listener.py
+++ ingest/listener.py
@@ -3,48 +3,51 @@ This module will listen to an email address and call Scraper.scrap once it
 received email
 """
 
-from Scraper import scrap
-import Thread
-import poplib
-from email import parser
+# from Scraper import scrap
+from threading import Thread
+import imaplib
+# from email import parser
 # def listen_notification():
 #    pass
 
 
 class EmailParser(object):
-    def __init__(self, host, user, password):
+    def __init__(self, host, mailbox, user, password):
         self.host = host
         self.user = user
         self.password = password
+        self.mailbox = mailbox
 
-    def GetEmails(self):
-        pop_conn = poplib.POP3_SSL(self.host)
-        pop_conn.user(self.user)
-        pop_conn.pass_(self.password)
-        msg_count = len(pop_conn.list()[1])
-        messages = []
-        p = parser.Parser()
-        for i in range(1, msg_count + 1):
-            msg = '\n'.join(pop_conn.retr(i)[1])
-            messages.push(p.parsestr(msg))
-        pop_conn.quit()
-        return messages
+    def GetEmails(self, search_filter='UNSEEN', query='(RFC822)', n=None):
+        try:
+            conn = imaplib.IMAP4_SSL(self.host)
+            conn.login(self.user, self.password)
+            conn.select(self.mailbox)
+            rv, data = conn.search(None, search_filter)
+            if rv != 'OK':
+                # TODO timestamp message, or just implement actual logging...
+                print('No messages found!\n')
+                return
+            labels = data[0].split()
+            count = len(labels)
+            n = count if n is None else n
+            n = min(n, count)
 
+            for num in labels[:n]:
+                rv, result = conn.fetch(num, query)
+                if rv != 'OK':
+                    raise RuntimeError('Failed to fetch message {}'
+                                       .format(num))
 
-class EmailMonitor(Thread):
-    def __init__(self, event, scrapeFunction, emailParser):
-        Thread.__init__(self)
-        self.stopped = event
-        self.scrape = scrapeFunction
-        self.emailParser = emailParser
+                seen_rv, seen_data = conn.store(num, '+FLAGS', '\\Seen')
 
-    def run(self):
-        while not self.stopped.wait(3600.0):
-            messages = self.emailParser.GetEmails()
-            for msg in messages:
-                self.scrape(msg)
+                yield result
+        finally:
+            conn.close()
+            conn.logout()
 
 
 if __name__ == '__main__':
-    p = EmailParser('pop.gmail.com', 'indivisible_bot', 'PASSWORD_GOES_HERE')
-    EmailMonitor(None, scrap, p)
+    p = EmailParser('imap.gmail.com', 'inbox', 'USERNAME', 'PASSWORD')
+    for msg in p.GetEmails():
+        print(msg)
