7db150cc2fc3448c45b3cbb80cf3e2ee4f8becd1
Aman Bhatia <aman.bhatia@outlook.com>
2017-09-01 05:42:33 -0700

    Refactor ResistanceNearMeScraper

--- ingest/web_scraper/resistancenearme.py
+++ ingest/web_scraper/resistancenearme.py
@@ -4,6 +4,13 @@ import logging
 
 
 class ResistanceNearMeScraper(AbstractWebScraper):
+    """
+    Scraper class for resistancenearme.org
+
+    Needs selenuim to load the webpage, so this class Implements its own
+    scrape() method, overriding the scrape() method from
+    AbstractWebScraper.
+    """
     _name = 'resistancenearme'
     _root_url = 'https://resistancenearme.org'
 
@@ -11,12 +18,91 @@ class ResistanceNearMeScraper(AbstractWebScraper):
         raise NotImplemented
 
     def extract_details(self, soup):
-        raise NotImplemented
+        """
+        Extract details of an event given the web-page.
+
+        Parameters
+        ----------
+        soup : BeautifulSoup
+            Soup of events webpage.
+
+        Returns
+        -------
+        details : dict
+            Dictionary containing event-info.
+        """
+        details = dict()
+        details['SOURCE'] = 'resistancenearme.org'
+        details['NOTES'] = ('Parsed {} for training data'
+                            .format(self._root_url))
+
+        event_url = soup.find('a', target='_self')
+        event_url = None if event_url is None else event_url['href']
+        details['URL'] = self._root_url + '/' + event_url
+
+        meta = soup.find('ul')
+
+        def read_meta(class_name, missing_value=None):
+            obj = meta.find(class_=class_name)
+            if obj is None:
+                return missing_value
+            else:
+                return obj.getText()
+
+        e_venue = read_meta('event-venue')
+        e_address = read_meta('event-address')
+        e_date = read_meta('event-date', '')
+        e_time = read_meta('event-time', '')
+
+        details['ORGANIZER'] = e_venue
+        details['LOCATION'] = e_address
+        details['DATE_TIME'] = e_date + e_time
+
+        event_name = soup.find(class_='event-name',
+                               **{'data-value': True})
+        event_type = soup.find(class_='event-type',
+                               **{'data-value': True})
+        event_name = '' if event_name is None else event_name['data-value']
+        event_type = '' if event_type is None else event_type['data-value']
+        description = soup['data-content']
+
+        details['NAME'] = event_name
+        details['TAGS'] = [event_type]
+        details['DESCRIPTION'] = description
+
+        #  Missing info
+        details['TYPES'] = []
+        details['LOCATION_GMAPS'] = None
+        details['SOCIAL'] = []
+
+        logging.debug('Scraped Data:\n'
+                      'Source: {SOURCE}\n'
+                      'Notes: {NOTES}\n'
+                      'Name: {NAME}\n'
+                      'Tags: {TAGS}\n'
+                      'Types: {TYPES}\n'
+                      'Location: {LOCATION_GMAPS} \t| {LOCATION}\n'
+                      'Social Links: {SOCIAL}\n'
+                      'Description:\n{DESCRIPTION}\n'
+                      'Date/Time: {DATE_TIME}\n'
+                      'Organizer: {ORGANIZER}\n'
+                      'URL: {URL}\n'.format(**details))
+        return details
 
     def scrape(self):
+        """
+        Scrape website for info on all events.
+        Details are extracted into a DataFrame object.
+
+        Returns
+        -------
+        event_df : pandas.core.frame.DataFrame
+            DataFrame object containing info for all events scraped.
+        """
         from selenium import webdriver
         import time
         import pandas as pd
+        from tqdm import tqdm
         browser = webdriver.PhantomJS()
         browser.set_window_size(1120, 550)
         browser.get(self._root_url)
@@ -36,7 +122,6 @@ class ResistanceNearMeScraper(AbstractWebScraper):
         event_dd.click()
         # next click on every event category in dropdown
         el = browser.find_elements_by_xpath('//a[@data-filter="meetingType"]')
-
         for element in el:
             time.sleep(3)
             element.click()
@@ -46,68 +131,11 @@ class ResistanceNearMeScraper(AbstractWebScraper):
         s = BeautifulSoup(browser.page_source, 'html.parser')
         # finds all events
         events = s.find_all(class_='event-row')
-
         df = pd.DataFrame()
         # for every event, append info to the proper list
-        for i in range(len(events)):
-            details = dict()
-            details['SOURCE'] = 'resistancenearme.org'
-            details['NOTES'] = ('Parsed {} for training data'
-                                .format(self._root_url))
-
-            event_url = events[i].find('a', target='_self')
-            event_url = None if event_url is None else event_url['href']
-            details['URL'] = self._root_url + '/' + event_url
-
-            meta = events[i].find('ul')
-
-            def read_meta(class_name, missing_value=None):
-                obj = meta.find(class_=class_name)
-                if obj is None:
-                    return missing_value
-                else:
-                    return obj.getText()
-
-            e_venue = read_meta('event-venue')
-            e_address = read_meta('event-address')
-            e_date = read_meta('event-date', '')
-            e_time = read_meta('event-time', '')
-
-            details['ORGANIZER'] = e_venue
-            details['LOCATION'] = e_address
-            details['DATE_TIME'] = e_date + e_time
-
-            event_name = events[i].find(class_='event-name',
-                                        **{'data-value': True})
-            event_type = events[i].find(class_='event-type',
-                                        **{'data-value': True})
-            event_name = '' if event_name is None else event_name['data-value']
-            event_type = '' if event_type is None else event_type['data-value']
-            description = events[i]['data-content']
-
-            details['NAME'] = event_name
-            details['TAGS'] = [event_type]
-            details['DESCRIPTION'] = description
-
-            #  Missing info
-            details['TYPES'] = []
-            details['LOCATION_GMAPS'] = None
-            details['SOCIAL'] = []
-
-            logging.debug('Scraped Data:\n'
-                          'Source: {SOURCE}\n'
-                          'Notes: {NOTES}\n'
-                          'Name: {NAME}\n'
-                          'Tags: {TAGS}\n'
-                          'Types: {TYPES}\n'
-                          'Location: {LOCATION_GMAPS} \t| {LOCATION}\n'
-                          'Social Links: {SOCIAL}\n'
-                          'Description:\n{DESCRIPTION}\n'
-                          'Date/Time: {DATE_TIME}\n'
-                          'Organizer: {ORGANIZER}\n'.format(**details))
-
+        for event_soup in tqdm(events, desc='Parsing Events'):
+            details = self.extract_details(event_soup)
             details['LAST_UPDATED'] = pd.Timestamp('now')
-            # TODO Add timezone to LAST_UPDATED ('now', tz='US/Pacific')
             df = df.append(details, ignore_index=True)
 
         return df
